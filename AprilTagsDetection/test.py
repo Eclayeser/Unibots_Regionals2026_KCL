e0553201d51c502a8938d9eeca6f18e9b60ec049
Mark Tarnavskyi <mark.tarnavskyy@gmail.com>
2026-03-10 02:16:44 +0000

    Testing folders - separated from final version for separate testing

---
+++ AprilTagsDetection/test.py
@@ -0,0 +1,2 @@
+#File for testing the image capturing with tags and obstacle detection.
+#Preferrably, run APF and AprilTagsDetection together to see the results of the APF vector on the ball detection.
